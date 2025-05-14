189aa68ad86029373fd17943ef426a3b601f0704
dishb <code.dishb@gmail.com>
2025-05-13 22:26:51 -0700

    chore: begin adding shadncn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import ThemeProvider from "@/components/themeProvider";
+import ThemeToggle from "@/components/themeToggle";
 import "./globals.css";
-
-const inter = Inter({
-  variable: "--font-inter",
-  weight: ["400", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Club Finance Tracker",
@@ -21,15 +14,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>
-        <div className="font-[family-name:var(--font-inter)]">
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ThemeToggle />
+
           <div className="flex flex-col min-h-screen">
             {/* Navbar goes here */}
             <main className="flex-1 flex flex-col">{children}</main>
           </div>
           {/* Footer goes here */}
-        </div>
+        </ThemeProvider>
       </body>
     </html>
   );
